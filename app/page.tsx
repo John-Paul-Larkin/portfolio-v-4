056889d3fb1448c5627a9dd68a297c28621a15ec
John Paul Larkin <johnplarkin@gmail.com>
2023-12-23 01:59:29 +0000

    create hero grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
+import HeroBannerName from "@/components/HeroBannerName";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">setup</main>;
+  return (
+    <main className=" bg-yellow-400">
+      <div className="max-w-container-width mx-auto min-h-screen... bg-white grid grid-rows-3 grid-cols-4 p-[2rem] gap-[2rem]">
+        <HeroBannerName />
+        <div className="row-span-1 bg-cream rounded-[2rem] pt-[100%] relative">
+          <div className="absolute top-0 left-0"> React Developer</div>
+        </div>
+        {/* <div className="row-span-1 bg-cream rounded-[2rem] pt-[100%] relative">
+          <div className="absolute top-0"> </div>
+        </div> */}
+
+        <div className="row-span-1 bg-cream rounded-[2rem] pt-[100%] relative"></div>
+        <div className="row-span-2 col-span-2 bg-red-600 rounded-[2rem] pt-[100%]... relative overflow-hidden">
+          <Image
+            src="/wrangler.png"
+            // height={4864}
+            // width={3648}
+            fill={true}
+            alt="JP Larkin stading in front of computer"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
+        <div></div>
+      </div>
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      maxWidth: {
+        "container-width": "100rem",
+      },
+      height: {
+        // "box-height": "21.875rem",
+        "box-height": "15.875rem",
+      },
+      colors: {
+        "overall-bg": "hsl(227, 28%, 43%)",
+        "box-bg": "hsl(111, 7%, 20%)",
+        cream: "hsl(0, 0%, 85%)",
+      },
+
+      gridTemplateRows: {
+        threeBox: "1fr 1fr 1fr",
+        destinationDesktop: "auto 1fr auto",
+        crewMobile: "auto 1fr auto",
+        crewDesktop: "auto 1fr",
+        crewDetails: "auto auto 1fr auto",
+        technologyMobile: "auto auto auto auto",
+        technologyDesktop: "auto 1fr",
+      },
+
+      gridTemplateColumns: {
+        fourBox: "1fr 1fr",
+        crewDesktop: "1fr 1fr",
+        technologyDesktop: "auto 1fr 1fr",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
